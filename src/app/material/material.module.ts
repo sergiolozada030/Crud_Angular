20382dd60d360558007d80e639c746faffa0e4a4
Sergio Lozada <salozada@indracompany.com>
2022-02-03 18:01:16 -0500

    importacion de recursos y ajustes en nav

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,10 +5,16 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+  ],
 })
 export class MaterialModule {}
