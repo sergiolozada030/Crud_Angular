168627cdd684dcc8374c237914e07f788b596f07
Sergio Lozada <salozada@indracompany.com>
2022-02-07 09:45:58 -0500

    consumo de la lista de heroes

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  styleUrls: ['./listado.component.css'],
 })
 export class ListadoComponent implements OnInit {
-
-  constructor() { }
+  constructor(private heroesServie: HeroesService) {}
 
   ngOnInit(): void {
+    this.heroesServie.getListaHeroes().subscribe((resp) => console.log(resp));
   }
-
 }
